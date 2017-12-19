d9ffc16dc31581faf5c1d5cb4101dca7e40dfa25
Centvrio <sytnic@gmail.com>
2017-12-19 11:41:50 +0200

    Face positive emoji

---
+++ src/Centvrio.Emoji/FacePositive.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Centvrio.Emoji
+{
+    public class FacePositive
+    {
+        /// <summary>
+        /// Grinning face 😀
+        /// </summary>
+        public static readonly EmojiString Grinning = 0x1f600;
+
+        /// <summary>
+        /// Beaming face with smiling eyes 😁
+        /// </summary>
+        public static readonly EmojiString Beaming = 0x1f601;
+
+        /// <summary>
+        /// Face with tears of joy 😂
+        /// </summary>
+        public static readonly EmojiString TearsOfJoy = 0x1f602;
+
+        /// <summary>
+        /// Rolling on the floor laughing 🤣
+        /// </summary>
+        public static readonly EmojiString FloorLaughing = 0x1f923;
+
+        /// <summary>
+        /// Grinning face with big eyes 😃
+        /// </summary>
+        public static readonly EmojiString BigEyes = 0x1f603;
+
+        /// <summary>
+        /// Grinning face with smiling eyes 😄
+        /// </summary>
+        public static readonly EmojiString GrinningSmilingEyes = 0x1f604;
+
+        /// <summary>
+        /// Grinning face with sweat 😅
+        /// </summary>
+        public static readonly EmojiString GrinningSweat = 0x1f605;
+
+        /// <summary>
+        /// Grinning squinting face 😆
+        /// </summary>
+        public static readonly EmojiString Squinting = 0x1f606;
+
+        /// <summary>
+        /// Winking face 😉
+        /// </summary>
+        public static readonly EmojiString Winking = 0x1f609;
+
+        /// <summary>
+        /// Smiling face with smiling eyes 😊
+        /// </summary>
+        public static readonly EmojiString SmilingSmilingEyes = 0x1f60A;
+
+        /// <summary>
+        /// Face savoring food 😋
+        /// </summary>
+        public static readonly EmojiString SavoringFood = 0x1f60b;
+
+        /// <summary>
+        /// Smiling face with sunglasses 😎
+        /// </summary>
+        public static readonly EmojiString Sunglasses = 0x1f60e;
+
+        /// <summary>
+        /// Smiling face with heart-eyes 😍
+        /// </summary>
+        public static readonly EmojiString HeartEyes = 0x1f60d;
+
+        /// <summary>
+        /// Face blowing a kiss 😘
+        /// </summary>
+        public static readonly EmojiString BlowingKiss = 0x1f618;
+
+        /// <summary>
+        /// Kissing face 😗
+        /// </summary>
+        public static readonly EmojiString Kissing = 0x1f617;
+
+        /// <summary>
+        /// Kissing face with smiling eyes 😙
+        /// </summary>
+        public static readonly EmojiString KissingSmilingEyes = 0x1f619;
+
+        /// <summary>
+        /// kissing face with closed eyes 😚
+        /// </summary>
+        public static readonly EmojiString KissingClosedEyes = 0x1f61a;
+
+        /// <summary>
+        /// Smiling face ☺
+        /// </summary>
+        public static readonly EmojiString Smiling = 0x263a;
+
+        /// <summary>
+        /// slightly smiling face 🙂
+        /// </summary>
+        public static readonly EmojiString Slightly = 0x1f642;
+
+        /// <summary>
+        /// hugging face 🤗
+        /// </summary>
+        public static readonly EmojiString Hugging = 0x1f917;
+
+        /// <summary>
+        /// Star-struck 🤩
+        /// </summary>
+        public static readonly EmojiString StarStruck = 0x1f929;
+    }
+}
