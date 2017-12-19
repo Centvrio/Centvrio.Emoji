02e69236d2bc4bc26380a9bb3b03d8949e132d31
Centvrio <sytnic@gmail.com>
2017-12-19 12:26:36 +0200

    Face Neutral emoji

---
+++ src/Centvrio.Emoji/FaceNeutral.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Centvrio.Emoji
+{
+    public class FaceNeutral
+    {
+        /// <summary>
+        /// Thinking face 🤔
+        /// </summary>
+        public static readonly EmojiString Thinking = 0x1f914;
+
+        /// <summary>
+        /// Face with raised eyebrow 🤨
+        /// </summary>
+        public static readonly EmojiString RaisedEyebrow = 0x1f928;
+
+        /// <summary>
+        /// Neutral face 😐
+        /// </summary>
+        public static readonly EmojiString Neutral = 0x1f610;
+
+        /// <summary>
+        /// Expressionless face 😑
+        /// </summary>
+        public static readonly EmojiString Expressionless = 0x1f611;
+
+        /// <summary>
+        /// Face without mouth 😶
+        /// </summary>
+        public static readonly EmojiString WithoutMouth = 0x1f636;
+
+        /// <summary>
+        /// Face with rolling eyes 🙄
+        /// </summary>
+        public static readonly EmojiString RollingEyes = 0x1f644;
+
+        /// <summary>
+        /// Smirking face 😏
+        /// </summary>
+        public static readonly EmojiString Smirking = 0x1f60f;
+
+        /// <summary>
+        /// Persevering face 😣
+        /// </summary>
+        public static readonly EmojiString Persevering = 0x1f623;
+
+        /// <summary>
+        /// Sad but relieved face 😥
+        /// </summary>
+        public static readonly EmojiString SadButRelieved = 0x1f625;
+
+        /// <summary>
+        /// Face with open mouth 😮
+        /// </summary>
+        public static readonly EmojiString OpenMouth = 0x1f62e;
+
+        /// <summary>
+        /// Zipper-mouth face 🤐
+        /// </summary>
+        public static readonly EmojiString ZipperMouth = 0x1f910;
+
+        /// <summary>
+        /// Hushed face 😯
+        /// </summary>
+        public static readonly EmojiString Hushed = 0x1f62f;
+
+        /// <summary>
+        /// Sleepy face 😪
+        /// </summary>
+        public static readonly EmojiString Sleepy = 0x1f62a;
+
+        /// <summary>
+        /// Tired face 😫
+        /// </summary>
+        public static readonly EmojiString Tired = 0x1f62b;
+
+        /// <summary>
+        /// Sleeping face 😴
+        /// </summary>
+        public static readonly EmojiString Sleeping = 0x1f634;
+
+        /// <summary>
+        /// Relieved face 😌
+        /// </summary>
+        public static readonly EmojiString Relieved = 0x1f60c;
+
+        /// <summary>
+        /// Face with tongue 😛
+        /// </summary>
+        public static readonly EmojiString FaceWithTongue = 0x1f61b;
+
+        /// <summary>
+        /// Winking face with tongue 😜
+        /// </summary>
+        public static readonly EmojiString WinkingWithTongue = 0x1f61c;
+
+        /// <summary>
+        /// Squinting face with tongue 😝
+        /// </summary>
+        public static readonly EmojiString SquintingWithTongue = 0x1f61d;
+
+        /// <summary>
+        /// Drooling face 🤤
+        /// </summary>
+        public static readonly EmojiString Drooling = 0x1f924;
+
+        /// <summary>
+        /// Unamused face 😒
+        /// </summary>
+        public static readonly EmojiString Unamused = 0x1f612;
+
+        /// <summary>
+        /// Downcast face with sweat 😓
+        /// </summary>
+        public static readonly EmojiString DowncastWithSweat = 0x1f613;
+
+        /// <summary>
+        /// Pensive face 😔
+        /// </summary>
+        public static readonly EmojiString Pensive = 0x1f614;
+
+        /// <summary>
+        /// Confused face 😕
+        /// </summary>
+        public static readonly EmojiString Confused = 0x1f615;
+
+        /// <summary>
+        /// Upside-down face 🙃
+        /// </summary>
+        public static readonly EmojiString UpsideDown = 0x1f643;
+
+        /// <summary>
+        /// Money-mouth face 🤑
+        /// </summary>
+        public static readonly EmojiString MoneyMouth = 0x1f911;
+
+        /// <summary>
+        /// Astonished face 😲
+        /// </summary>
+        public static readonly EmojiString Astonished = 0x1f632;
+    }
+}
