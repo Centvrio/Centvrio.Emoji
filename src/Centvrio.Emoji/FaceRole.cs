394f08eab3344dee9b1a45db9ff09266c3b81f1a
Centvrio <sytnic@gmail.com>
2017-12-19 13:56:26 +0200

    Face role emoji. Close #6

---
+++ src/Centvrio.Emoji/FaceRole.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Centvrio.Emoji
+{
+    public class FaceRole
+    {
+        /// <summary>
+        /// Smiling face with halo 😇
+        /// </summary>
+        public static readonly EmojiString SmilingWithHalo = 0x1F607;
+
+        /// <summary>
+        /// Cowboy hat face 🤠
+        /// </summary>
+        public static readonly EmojiString CowboyHat = 0x1F920;
+
+        /// <summary>
+        /// Clown face 🤡
+        /// </summary>
+        public static readonly EmojiString Clown = 0x1F921;
+
+        /// <summary>
+        /// Lying face 🤥
+        /// </summary>
+        public static readonly EmojiString Lying = 0x1F925;
+
+        /// <summary>
+        /// Shushing face 🤫
+        /// </summary>
+        public static readonly EmojiString Shushing = 0x1F92B;
+
+        /// <summary>
+        /// Face with hand over mouth 🤭
+        /// </summary>
+        public static readonly EmojiString HandOverMouth = 0x1F92D;
+
+        /// <summary>
+        /// Face with monocle 🧐
+        /// </summary>
+        public static readonly EmojiString Monocle = 0x1F9D0;
+
+        /// <summary>
+        /// Nerd face 🤓
+        /// </summary>
+        public static readonly EmojiString Nerd = 0x1F913;
+    }
+}
