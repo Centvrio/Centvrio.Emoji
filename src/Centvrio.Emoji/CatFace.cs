4cf426344ea3b14acd5c6894291ae5ee3a238b25
Centvrio <sytnic@gmail.com>
2017-12-20 10:26:24 +0200

    Add cat face emoji. Close #11

---
+++ src/Centvrio.Emoji/CatFace.cs
@@ -0,0 +1,50 @@
+﻿namespace Centvrio.Emoji
+{
+    public class CatFace
+    {
+        /// <summary>
+        /// Grinning cat face 😺
+        /// </summary>
+        public static readonly EmojiString Grinning = 0x1F63A;
+
+        /// <summary>
+        /// Grinning cat face with smiling eyes 😸
+        /// </summary>
+        public static readonly EmojiString GrinningSmilingEyes = 0x1F638;
+
+        /// <summary>
+        /// Cat face with tears of joy 😹
+        /// </summary>
+        public static readonly EmojiString TearsOfJoy = 0x1F639;
+
+        /// <summary>
+        /// Smiling cat face with heart-eyes 😻
+        /// </summary>
+        public static readonly EmojiString HeartEyes = 0x1F63B;
+
+        /// <summary>
+        /// Cat face with wry smile 😼
+        /// </summary>
+        public static readonly EmojiString WrySmile = 0x1F63C;
+
+        /// <summary>
+        /// Kissing cat face 😽
+        /// </summary>
+        public static readonly EmojiString Kissing = 0x1F63D;
+
+        /// <summary>
+        /// Weary cat face 🙀
+        /// </summary>
+        public static readonly EmojiString Weary = 0x1F640;
+
+        /// <summary>
+        /// Crying cat face 😿
+        /// </summary>
+        public static readonly EmojiString Crying = 0x1F63F;
+
+        /// <summary>
+        /// Pouting cat face 😾
+        /// </summary>
+        public static readonly EmojiString Pouting = 0x1F63E;
+    }
+}
