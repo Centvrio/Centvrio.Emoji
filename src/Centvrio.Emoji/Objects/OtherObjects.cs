b4bbda520b321a83f3babfadf8a6134aa7a630fe
Centvrio <sytnic@gmail.com>
2017-12-20 16:54:59 +0200

    Lock emoji Medical emoji Other objects emoji

---
+++ src/Centvrio.Emoji/Objects/OtherObjects.cs
@@ -0,0 +1,40 @@
+﻿namespace Centvrio.Emoji
+{
+    public class OtherObjects
+    {
+        /// <summary>
+        /// Cigarette 🚬
+        /// </summary>
+        public static readonly EmojiString Cigarette = 0x1F6AC;
+
+        /// <summary>
+        /// Coffin ⚰
+        /// </summary>
+        public static readonly EmojiString Coffin = 0x26B0;
+
+        /// <summary>
+        /// Funeral urn ⚱
+        /// </summary>
+        public static readonly EmojiString FuneralUrn = 0x26B1;
+
+        /// <summary>
+        /// Moai 🗿
+        /// </summary>
+        public static readonly EmojiString Moai = 0x1F5FF;
+
+        /// <summary>
+        /// Oil drum 🛢
+        /// </summary>
+        public static readonly EmojiString OilDrum = 0x1F6E2;
+
+        /// <summary>
+        /// Crystal ball 🔮
+        /// </summary>
+        public static readonly EmojiString CrystalBall = 0x1F52E;
+
+        /// <summary>
+        /// Shopping cart 🛒
+        /// </summary>
+        public static readonly EmojiString ShoppingCart = 0x1F6D2;
+    }
+}
