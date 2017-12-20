44faec604e8ca5dcf616adff518b3c690abe7669
Centvrio <sytnic@gmail.com>
2017-12-20 12:12:11 +0200

    Sound emoji

---
+++ src/Centvrio.Emoji/Sound.cs
@@ -0,0 +1,50 @@
+﻿namespace Centvrio.Emoji
+{
+    public class Sound
+    {
+        /// <summary>
+        /// Muted speaker 🔇
+        /// </summary>
+        public static readonly EmojiString Muted = 0x1F507;
+
+        /// <summary>
+        /// Speaker low volume 🔈
+        /// </summary>
+        public static readonly EmojiString LowVolumex = 0x1F508;
+
+        /// <summary>
+        /// Speaker medium volume 🔉
+        /// </summary>
+        public static readonly EmojiString MediumVolume = 0x1F509;
+
+        /// <summary>
+        /// Speaker high volume 🔊
+        /// </summary>
+        public static readonly EmojiString HighVolume = 0x1F50A;
+
+        /// <summary>
+        /// Loudspeaker 📢
+        /// </summary>
+        public static readonly EmojiString Loudspeaker = 0x1F4E2;
+
+        /// <summary>
+        /// Megaphone 📣
+        /// </summary>
+        public static readonly EmojiString Megaphone = 0x1F4E3;
+
+        /// <summary>
+        /// Postal horn 📯
+        /// </summary>
+        public static readonly EmojiString PostalHorn = 0x1F4EF;
+
+        /// <summary>
+        /// Bell 🔔
+        /// </summary>
+        public static readonly EmojiString Bell = 0x1F514;
+
+        /// <summary>
+        /// Bell with slash 🔕
+        /// </summary>
+        public static readonly EmojiString BellWithSlash = 0x1F515;
+    }
+}
