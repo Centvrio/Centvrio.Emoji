00ad86dbe5c05c39135445fb55aa2efa6ff86fa2
Centvrio <sytnic@gmail.com>
2017-12-20 16:36:15 +0200

    Computer emoji Light & video emoji

---
+++ src/Centvrio.Emoji/Objects/LightVideo.cs
@@ -0,0 +1,96 @@
+﻿namespace Centvrio.Emoji
+{
+    public class LightVideo
+    {
+        /// <summary>
+        /// movie camera 🎥
+        /// </summary>
+        public static readonly EmojiString MovieCamera = 0x1F3A5;
+
+        /// <summary>
+        /// film frames 🎞
+        /// </summary>
+        public static readonly EmojiString Frames = 0x1F39E;
+
+        /// <summary>
+        /// film projector 📽
+        /// </summary>
+        public static readonly EmojiString FilmProjector = 0x1F4FD;
+
+        /// <summary>
+        /// clapper board 🎬
+        /// </summary>
+        public static readonly EmojiString ClapperBoard = 0x1F3AC;
+
+        /// <summary>
+        /// television 📺
+        /// </summary>
+        public static readonly EmojiString Television = 0x1F4FA;
+
+        /// <summary>
+        /// camera 📷
+        /// </summary>
+        public static readonly EmojiString Camera = 0x1F4F7;
+
+        /// <summary>
+        /// camera with flash 📸
+        /// </summary>
+        public static readonly EmojiString CameraWithFlash = 0x1F4F8;
+
+        /// <summary>
+        /// video camera 📹
+        /// </summary>
+        public static readonly EmojiString VideoCamera = 0x1F4F9;
+
+        /// <summary>
+        /// videocassette 📼
+        /// </summary>
+        public static readonly EmojiString Videocassette = 0x1F4FC;
+
+        /// <summary>
+        /// magnifying glass tilted left 🔍
+        /// </summary>
+        public static readonly EmojiString GlassLeft = 0x1F50D;
+
+        /// <summary>
+        /// magnifying glass tilted right 🔎
+        /// </summary>
+        public static readonly EmojiString GlassRight = 0x1F50E;
+
+        /// <summary>
+        /// microscope 🔬
+        /// </summary>
+        public static readonly EmojiString Microscope = 0x1F52C;
+
+        /// <summary>
+        /// telescope 🔭
+        /// </summary>
+        public static readonly EmojiString Telescope = 0x1F52D;
+
+        /// <summary>
+        /// satellite antenna 📡
+        /// </summary>
+        public static readonly EmojiString SatelliteAntenna = 0x1F4E1;
+
+        /// <summary>
+        /// candle 🕯
+        /// </summary>
+        public static readonly EmojiString Candle = 0x1F56F;
+
+        /// <summary>
+        /// light bulb 💡
+        /// </summary>
+        public static readonly EmojiString LightBulb = 0x1F4A1;
+
+        /// <summary>
+        /// flashlight 🔦
+        /// </summary>
+        public static readonly EmojiString Flashlight = 0x1F526;
+
+        /// <summary>
+        /// red paper lantern 🏮
+        /// </summary>
+        public static readonly EmojiString RedPaperLantern = 0x1F3EE;
+
+    }
+}
