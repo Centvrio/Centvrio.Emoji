3095733593ef62e1ee94e8d83754bbbb2f09f69c
Centvrio <sytnic@gmail.com>
2018-01-04 20:33:27 +0200

    Add subgroups: place-map place-geographic

---
+++ src/Centvrio.Emoji/TravelPlaces/PlaceMap.cs
@@ -0,0 +1,35 @@
+﻿namespace Centvrio.Emoji
+{
+    public class PlaceMap
+    {
+        /// <summary>
+        /// Globe showing Europe-Africa 🌍
+        /// </summary>
+        public static readonly EmojiString EuropeAfrica = 0x1F30D;
+
+        /// <summary>
+        /// Globe showing Americas 🌎
+        /// </summary>
+        public static readonly EmojiString Americas = 0x1F30E;
+
+        /// <summary>
+        /// Globe showing Asia-Australia 🌏
+        /// </summary>
+        public static readonly EmojiString AsiaAustralia = 0x1F30F;
+
+        /// <summary>
+        /// Globe with meridians 🌐
+        /// </summary>
+        public static readonly EmojiString GlobeMeridians = 0x1F310;
+
+        /// <summary>
+        /// World map 🗺
+        /// </summary>
+        public static readonly EmojiString WorldMap = 0x1F5FA;
+
+        /// <summary>
+        /// Map of Japan 🗾
+        /// </summary>
+        public static readonly EmojiString Japan = 0x1F5FE;
+    }
+}
