46370a01fdddc6ca6cf400a56eed40ac6a5db484
Centvrio <sytnic@gmail.com>
2018-01-06 11:19:07 +0200

    Variation Selectors

---
+++ src/Centvrio.Emoji/VariationSelectors.cs
@@ -0,0 +1,85 @@
+﻿namespace Centvrio.Emoji
+{
+    public class VariationSelectors
+    {
+        /// <summary>
+        /// VARIATION SELECTOR-1
+        /// </summary>
+        public static readonly EmojiString VS1 = 0xFE00;
+
+        /// <summary>
+        /// VARIATION SELECTOR-2
+        /// </summary>
+        public static readonly EmojiString VS2 = 0xFE01;
+
+        /// <summary>
+        /// VARIATION SELECTOR-3
+        /// </summary>
+        public static readonly EmojiString VS3 = 0xFE02;
+
+        /// <summary>
+        /// VARIATION SELECTOR-4
+        /// </summary>
+        public static readonly EmojiString VS4 = 0xFE03;
+
+        /// <summary>
+        /// VARIATION SELECTOR-5
+        /// </summary>
+        public static readonly EmojiString VS5 = 0xFE04;
+
+        /// <summary>
+        /// VARIATION SELECTOR-6
+        /// </summary>
+        public static readonly EmojiString VS6 = 0xFE05;
+
+        /// <summary>
+        /// VARIATION SELECTOR-7
+        /// </summary>
+        public static readonly EmojiString VS7 = 0xFE06;
+
+        /// <summary>
+        /// VARIATION SELECTOR-8
+        /// </summary>
+        public static readonly EmojiString VS8 = 0xFE07;
+
+        /// <summary>
+        /// VARIATION SELECTOR-9
+        /// </summary>
+        public static readonly EmojiString VS9 = 0xFE08;
+
+        /// <summary>
+        /// VARIATION SELECTOR-10
+        /// </summary>
+        public static readonly EmojiString VS10 = 0xFE09;
+
+        /// <summary>
+        /// VARIATION SELECTOR-11
+        /// </summary>
+        public static readonly EmojiString VS11 = 0xFE0A;
+
+        /// <summary>
+        /// VARIATION SELECTOR-12
+        /// </summary>
+        public static readonly EmojiString VS12 = 0xFE0B;
+
+        /// <summary>
+        /// VARIATION SELECTOR-13
+        /// </summary>
+        public static readonly EmojiString VS13 = 0xFE0C;
+
+        /// <summary>
+        /// VARIATION SELECTOR-14
+        /// </summary>
+        public static readonly EmojiString VS14 = 0xFE0D;
+
+        /// <summary>
+        /// VARIATION SELECTOR-15
+        /// </summary>
+        public static readonly EmojiString VS15 = 0xFE0E;
+
+        /// <summary>
+        /// VARIATION SELECTOR-16
+        /// </summary>
+        public static readonly EmojiString VS16 = 0xFE0E;
+    }
+}
