dca39012657efdbf1334bf773fd996a09a2c66ad
Centvrio <sytnic@gmail.com>
2018-01-09 14:35:14 +0200

    Close #28

---
+++ src/Centvrio.Emoji/ModifierFitzpatrick.cs
@@ -0,0 +1,30 @@
+﻿namespace Centvrio.Emoji
+{
+    public class ModifierFitzpatrick
+    {
+        /// <summary>
+        /// Emoji Modifier Fitzpatrick Type-1-2
+        /// </summary>
+        public static readonly UnicodeString Type1 = 0x1F3FB;
+
+        /// <summary>
+        /// Emoji Modifier Fitzpatrick Type-3
+        /// </summary>
+        public static readonly UnicodeString Type3 = 0x1F3FC;
+
+        /// <summary>
+        /// Emoji Modifier Fitzpatrick Type-4
+        /// </summary>
+        public static readonly UnicodeString Type4 = 0x1F3FD;
+
+        /// <summary>
+        /// Emoji Modifier Fitzpatrick Type-5
+        /// </summary>
+        public static readonly UnicodeString Type5 = 0x1F3FE;
+
+        /// <summary>
+        /// Emoji Modifier Fitzpatrick Type-6
+        /// </summary>
+        public static readonly UnicodeString Type6 = 0x1F3FF;
+    }
+}
