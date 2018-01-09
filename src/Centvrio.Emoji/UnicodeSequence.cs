7e9270ef7e8e864dbd348c8a52248bc4f4b8608f
Centvrio <sytnic@gmail.com>
2018-01-09 14:29:41 +0200

    Close #24

--- src/Centvrio.Emoji/UnicodeSequence.cs
+++ src/Centvrio.Emoji/UnicodeSequence.cs
@@ -9,18 +9,31 @@ namespace Centvrio.Emoji
     {
         private readonly IList<UnicodeString> unicodeStrings;
 
+        /// <summary>
+        /// Initializes a new instance of Centvrio.Emoji.UnicodeSequence
+        /// </summary>
+        /// <param name="capacity">Number of elements the internal sequence</param>
+        /// <exception cref="ArgumentOutOfRangeException">Capacity is less than 0.</exception>
         public UnicodeSequence(int capacity)
         {
             if (capacity <= 0)
             {
-                throw new ArgumentOutOfRangeException("Capacity is less than 0");
+                throw new ArgumentOutOfRangeException("Capacity is less than 0.");
             }
             Capacity = capacity;
             unicodeStrings = new List<UnicodeString>(capacity);
         }
 
+        /// <summary>
+        /// Gets or sets the total number of elements the internal sequence.
+        /// </summary>
         public int Capacity { get; private set; }
 
+        /// <summary>
+        /// Add item to Centvrio.Emoji.UnicodeSequence.
+        /// </summary>
+        /// <param name="unicodeString">Centvrio.Emoji.UnicodeString instance</param>
+        /// <returns>Current Centvrio.Emoji.UnicodeSequence instance</returns>
         public UnicodeSequence Add(UnicodeString unicodeString)
         {
             unicodeStrings.Add(unicodeString);
