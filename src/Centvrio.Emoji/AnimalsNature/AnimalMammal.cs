16a136e4c120e922b190d61ff00330a6cc7dd09f
Centvrio <sytnic@gmail.com>
2018-01-09 13:10:05 +0200

    Add subgroup: animal-mammal animal-bird

---
+++ src/Centvrio.Emoji/AnimalsNature/AnimalMammal.cs
@@ -0,0 +1,245 @@
+﻿namespace Centvrio.Emoji
+{
+    public class AnimalMammal
+    {
+        /// <summary>
+        /// Monkey face 🐵
+        /// </summary>
+        public static readonly UnicodeString MonkeyFace = 0x1F435;
+
+        /// <summary>
+        /// Monkey 🐒
+        /// </summary>
+        public static readonly UnicodeString Monkey = 0x1F412;
+
+        /// <summary>
+        /// Gorilla 🦍
+        /// </summary>
+        public static readonly UnicodeString Gorilla = 0x1F98D;
+
+        /// <summary>
+        /// Dog face 🐶
+        /// </summary>
+        public static readonly UnicodeString DogFace = 0x1F436;
+
+        /// <summary>
+        /// Dog 🐕
+        /// </summary>
+        public static readonly UnicodeString Dog = 0x1F415;
+
+        /// <summary>
+        /// Poodle 🐩
+        /// </summary>
+        public static readonly UnicodeString Poodle = 0x1F429;
+
+        /// <summary>
+        /// Wolf face 🐺
+        /// </summary>
+        public static readonly UnicodeString WolfFace = 0x1F43A;
+
+        /// <summary>
+        /// Fox face 🦊
+        /// </summary>
+        public static readonly UnicodeString FoxFace = 0x1F98A;
+
+        /// <summary>
+        /// Cat face 🐱
+        /// </summary>
+        public static readonly UnicodeString CatFace = 0x1F431;
+
+        /// <summary>
+        /// Cat 🐈
+        /// </summary>
+        public static readonly UnicodeString Cat = 0x1F408;
+
+        /// <summary>
+        /// Lion face 🦁
+        /// </summary>
+        public static readonly UnicodeString LionFace = 0x1F981;
+
+        /// <summary>
+        /// Tiger face 🐯
+        /// </summary>
+        public static readonly UnicodeString TigerFace = 0x1F42F;
+
+        /// <summary>
+        /// Tiger 🐅
+        /// </summary>
+        public static readonly UnicodeString Tiger = 0x1F405;
+
+        /// <summary>
+        /// Leopard 🐆
+        /// </summary>
+        public static readonly UnicodeString Leopard = 0x1F406;
+
+        /// <summary>
+        /// Horse face 🐴
+        /// </summary>
+        public static readonly UnicodeString HorseFace = 0x1F434;
+
+        /// <summary>
+        /// Horse 🐎
+        /// </summary>
+        public static readonly UnicodeString Horse = 0x1F40E;
+
+        /// <summary>
+        /// Unicorn face 🦄
+        /// </summary>
+        public static readonly UnicodeString UnicornFace = 0x1F984;
+
+        /// <summary>
+        /// Zebra 🦓
+        /// </summary>
+        public static readonly UnicodeString Zebra = 0x1F993;
+
+        /// <summary>
+        /// Deer 🦌
+        /// </summary>
+        public static readonly UnicodeString Deer = 0x1F98C;
+
+        /// <summary>
+        /// Cow face 🐮
+        /// </summary>
+        public static readonly UnicodeString CowFace = 0x1F42E;
+
+        /// <summary>
+        /// Ox 🐂
+        /// </summary>
+        public static readonly UnicodeString Ox = 0x1F402;
+
+        /// <summary>
+        /// Water buffalo 🐃
+        /// </summary>
+        public static readonly UnicodeString WaterBuffalo = 0x1F403;
+
+        /// <summary>
+        /// Cow 🐄
+        /// </summary>
+        public static readonly UnicodeString Cow = 0x1F404;
+
+        /// <summary>
+        /// Pig face 🐷
+        /// </summary>
+        public static readonly UnicodeString PigFace = 0x1F437;
+
+        /// <summary>
+        /// Pig 🐖
+        /// </summary>
+        public static readonly UnicodeString Pig = 0x1F416;
+
+        /// <summary>
+        /// Boar 🐗
+        /// </summary>
+        public static readonly UnicodeString Boar = 0x1F417;
+
+        /// <summary>
+        /// Pig nose 🐽
+        /// </summary>
+        public static readonly UnicodeString PigNose = 0x1F43D;
+
+        /// <summary>
+        /// Ram 🐏
+        /// </summary>
+        public static readonly UnicodeString Ram = 0x1F40F;
+
+        /// <summary>
+        /// Ewe 🐑
+        /// </summary>
+        public static readonly UnicodeString Ewe = 0x1F411;
+
+        /// <summary>
+        /// Goat 🐐
+        /// </summary>
+        public static readonly UnicodeString Goat = 0x1F410;
+
+        /// <summary>
+        /// Camel 🐪
+        /// </summary>
+        public static readonly UnicodeString Camel = 0x1F42A;
+
+        /// <summary>
+        /// Two-hump camel 🐫
+        /// </summary>
+        public static readonly UnicodeString TwoHumpCamel = 0x1F42B;
+
+        /// <summary>
+        /// Giraffe 🦒
+        /// </summary>
+        public static readonly UnicodeString Giraffe = 0x1F992;
+
+        /// <summary>
+        /// Elephant 🐘
+        /// </summary>
+        public static readonly UnicodeString Elephant = 0x1F418;
+
+        /// <summary>
+        /// Rhinoceros 🦏
+        /// </summary>
+        public static readonly UnicodeString Rhinoceros = 0x1F98F;
+
+        /// <summary>
+        /// Mouse face 🐭
+        /// </summary>
+        public static readonly UnicodeString MouseFace = 0x1F42D;
+
+        /// <summary>
+        /// Mouse 🐁
+        /// </summary>
+        public static readonly UnicodeString Mouse = 0x1F401;
+
+        /// <summary>
+        /// Rat 🐀
+        /// </summary>
+        public static readonly UnicodeString Rat = 0x1F400;
+
+        /// <summary>
+        /// Hamster face 🐹
+        /// </summary>
+        public static readonly UnicodeString HamsterFace = 0x1F439;
+
+        /// <summary>
+        /// Rabbit face 🐰
+        /// </summary>
+        public static readonly UnicodeString RabbitFace = 0x1F430;
+
+        /// <summary>
+        /// Rabbit 🐇
+        /// </summary>
+        public static readonly UnicodeString Rabbit = 0x1F407;
+
+        /// <summary>
+        /// Chipmunk 🐿️
+        /// </summary>
+        public static readonly UnicodeString Chipmunk = 0x1F43F;
+
+        /// <summary>
+        /// Hedgehog 🦔
+        /// </summary>
+        public static readonly UnicodeString Hedgehog = 0x1F994;
+
+        /// <summary>
+        /// bat 🦇
+        /// </summary>
+        public static readonly UnicodeString Bat = 0x1F987;
+
+        /// <summary>
+        /// Bear face 🐻
+        /// </summary>
+        public static readonly UnicodeString BearFace = 0x1F43B;
+
+        /// <summary>
+        /// Koala 🐨
+        /// </summary>
+        public static readonly UnicodeString Koala = 0x1F428;
+
+        /// <summary>
+        /// Panda face 🐼
+        /// </summary>
+        public static readonly UnicodeString PandaFace = 0x1F43C;
+
+        /// <summary>
+        /// Paw prints 🐾
+        /// </summary>
+        public static readonly UnicodeString PawPrints = 0x1F43E;
+    }
+}
