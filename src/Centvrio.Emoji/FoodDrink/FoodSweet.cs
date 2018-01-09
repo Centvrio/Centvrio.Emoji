dee730baabee1fc820987fdec9a7ca043cf676b2
Centvrio <sytnic@gmail.com>
2018-01-09 12:57:52 +0200

    Add subgroups: food-sweet drink dishware

---
+++ src/Centvrio.Emoji/FoodDrink/FoodSweet.cs
@@ -0,0 +1,70 @@
+﻿namespace Centvrio.Emoji
+{
+    public class FoodSweet
+    {
+        /// <summary>
+        /// Soft ice cream 🍦
+        /// </summary>
+        public static readonly UnicodeString SoftIceCream = 0x1F366;
+
+        /// <summary>
+        /// Shaved ice 🍧
+        /// </summary>
+        public static readonly UnicodeString ShavedIce = 0x1F367;
+
+        /// <summary>
+        /// Ice cream 🍨
+        /// </summary>
+        public static readonly UnicodeString IceCream = 0x1F368;
+
+        /// <summary>
+        /// Doughnut 🍩
+        /// </summary>
+        public static readonly UnicodeString Doughnut = 0x1F369;
+
+        /// <summary>
+        /// Cookie 🍪
+        /// </summary>
+        public static readonly UnicodeString Cookie = 0x1F36A;
+
+        /// <summary>
+        /// Birthday cake 🎂
+        /// </summary>
+        public static readonly UnicodeString BirthdayCake = 0x1F382;
+
+        /// <summary>
+        /// Shortcake 🍰
+        /// </summary>
+        public static readonly UnicodeString Shortcake = 0x1F370;
+
+        /// <summary>
+        /// Pie 🥧
+        /// </summary>
+        public static readonly UnicodeString Pie = 0x1F967;
+
+        /// <summary>
+        /// Chocolate bar 🍫
+        /// </summary>
+        public static readonly UnicodeString ChocolateBar = 0x1F36B;
+
+        /// <summary>
+        /// Candy 🍬
+        /// </summary>
+        public static readonly UnicodeString Candy = 0x1F36C;
+
+        /// <summary>
+        /// Lollipop 🍭
+        /// </summary>
+        public static readonly UnicodeString Lollipop = 0x1F36D;
+
+        /// <summary>
+        /// Custard 🍮
+        /// </summary>
+        public static readonly UnicodeString Custard = 0x1F36E;
+
+        /// <summary>
+        /// Honey pot 🍯
+        /// </summary>
+        public static readonly UnicodeString HoneyPot = 0x1F36F;
+    }
+}
