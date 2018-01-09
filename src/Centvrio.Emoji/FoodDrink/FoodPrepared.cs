341565e2273963a005296b07b17fec4b99775ca5
Centvrio <sytnic@gmail.com>
2018-01-09 12:08:02 +0200

    Add subgroups: food-prepared food-asian

---
+++ src/Centvrio.Emoji/FoodDrink/FoodPrepared.cs
@@ -0,0 +1,135 @@
+﻿namespace Centvrio.Emoji
+{
+    public class FoodPrepared
+    {
+        /// <summary>
+        /// Bread 🍞
+        /// </summary>
+        public static readonly UnicodeString Bread = 0x1F35E;
+
+        /// <summary>
+        /// Croissant 🥐
+        /// </summary>
+        public static readonly UnicodeString Croissant = 0x1F950;
+
+        /// <summary>
+        /// Baguette bread 🥖
+        /// </summary>
+        public static readonly UnicodeString BaguetteBread = 0x1F956;
+
+        /// <summary>
+        /// Pretzel 🥨
+        /// </summary>
+        public static readonly UnicodeString Pretzel = 0x1F968;
+
+        /// <summary>
+        /// pancakes 🥞
+        /// </summary>
+        public static readonly UnicodeString Pancakes = 0x1F95E;
+
+        /// <summary>
+        /// Cheese wedge 🧀
+        /// </summary>
+        public static readonly UnicodeString CheeseWedge = 0x1F9C0;
+
+        /// <summary>
+        /// Meat on bone 🍖
+        /// </summary>
+        public static readonly UnicodeString MeatOnBone = 0x1F356;
+
+        /// <summary>
+        /// Poultry leg 🍗
+        /// </summary>
+        public static readonly UnicodeString PoultryLeg = 0x1F357;
+
+        /// <summary>
+        /// Cut of meat 🥩
+        /// </summary>
+        public static readonly UnicodeString CutOfMeat = 0x1F969;
+
+        /// <summary>
+        /// Bacon 🥓
+        /// </summary>
+        public static readonly UnicodeString Bacon = 0x1F953;
+
+        /// <summary>
+        /// Hamburger 🍔
+        /// </summary>
+        public static readonly UnicodeString Hamburger = 0x1F354;
+
+        /// <summary>
+        /// French fries 🍟
+        /// </summary>
+        public static readonly UnicodeString FrenchFries = 0x1F35F;
+
+        /// <summary>
+        /// Pizza 🍕
+        /// </summary>
+        public static readonly UnicodeString Pizza = 0x1F355;
+
+        /// <summary>
+        /// Hot dog 🌭
+        /// </summary>
+        public static readonly UnicodeString HotDog = 0x1F32D;
+
+        /// <summary>
+        /// sandwich 🥪
+        /// </summary>
+        public static readonly UnicodeString Sandwich = 0x1F96A;
+
+        /// <summary>
+        /// Taco 🌮
+        /// </summary>
+        public static readonly UnicodeString Taco = 0x1F32E;
+
+        /// <summary>
+        /// Burrito 🌯
+        /// </summary>
+        public static readonly UnicodeString Burrito = 0x1F32F;
+
+        /// <summary>
+        /// Stuffed flatbread 🥙
+        /// </summary>
+        public static readonly UnicodeString StuffedFlatbread = 0x1F959;
+
+        /// <summary>
+        /// Egg 🥚
+        /// </summary>
+        public static readonly UnicodeString Egg = 0x1F95A;
+
+        /// <summary>
+        /// Cooking 🍳
+        /// </summary>
+        public static readonly UnicodeString Cooking = 0x1F373;
+
+        /// <summary>
+        /// Shallow pan of food 🥘
+        /// </summary>
+        public static readonly UnicodeString ShallowPanOfFood = 0x1F958;
+
+        /// <summary>
+        /// Pot of food 🍲
+        /// </summary>
+        public static readonly UnicodeString PotOfFood = 0x1F372;
+
+        /// <summary>
+        /// Bowl with spoon 🥣
+        /// </summary>
+        public static readonly UnicodeString BowlWithSpoon = 0x1F963;
+
+        /// <summary>
+        /// Green salad 🥗
+        /// </summary>
+        public static readonly UnicodeString GreenSalad = 0x1F957;
+
+        /// <summary>
+        /// Popcorn 🍿
+        /// </summary>
+        public static readonly UnicodeString Popcorn = 0x1F37F;
+
+        /// <summary>
+        /// Canned food 🥫
+        /// </summary>
+        public static readonly UnicodeString CannedFood = 0x1F96B;
+    }
+}
