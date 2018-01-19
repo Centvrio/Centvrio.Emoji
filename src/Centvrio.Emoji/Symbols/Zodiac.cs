f4880ac406a4ede5ff957dc542f16763e5e50678
Centvrio <sytnic@gmail.com>
2018-01-19 16:37:17 +0200

    Add subgroups: religion zodiac

---
+++ src/Centvrio.Emoji/Symbols/Zodiac.cs
@@ -0,0 +1,70 @@
+﻿namespace Centvrio.Emoji
+{
+    public class Zodiac
+    {
+        /// <summary>
+        /// Aries ♈
+        /// </summary>
+        public static readonly UnicodeString Aries = 0x2648;
+
+        /// <summary>
+        /// Taurus ♉
+        /// </summary>
+        public static readonly UnicodeString Taurus = 0x2649;
+
+        /// <summary>
+        /// Gemini ♊
+        /// </summary>
+        public static readonly UnicodeString Gemini = 0x264A;
+
+        /// <summary>
+        /// Cancer ♋
+        /// </summary>
+        public static readonly UnicodeString Cancer = 0x264B;
+
+        /// <summary>
+        /// Leo ♌
+        /// </summary>
+        public static readonly UnicodeString Leo = 0x264C;
+
+        /// <summary>
+        /// Virgo ♍
+        /// </summary>
+        public static readonly UnicodeString Virgo = 0x264D;
+
+        /// <summary>
+        /// Libra ♎
+        /// </summary>
+        public static readonly UnicodeString Libra = 0x264E;
+
+        /// <summary>
+        /// Scorpius ♏
+        /// </summary>
+        public static readonly UnicodeString Scorpius = 0x264F;
+
+        /// <summary>
+        /// Sagittarius ♐
+        /// </summary>
+        public static readonly UnicodeString Sagittarius = 0x2650;
+
+        /// <summary>
+        /// Capricorn ♑
+        /// </summary>
+        public static readonly UnicodeString Capricorn = 0x2651;
+
+        /// <summary>
+        /// Aquarius ♒
+        /// </summary>
+        public static readonly UnicodeString Aquarius = 0x2652;
+
+        /// <summary>
+        /// Pisces ♓
+        /// </summary>
+        public static readonly UnicodeString Pisces = 0x2653;
+
+        /// <summary>
+        /// Ophiuchus ⛎
+        /// </summary>
+        public static readonly UnicodeString Ophiuchus = 0x26CE;
+    }
+}
