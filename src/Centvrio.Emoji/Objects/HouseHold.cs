d6a5741ac7818b02ccc97fd6651993812fbdad4a
Centvrio <sytnic@gmail.com>
2018-02-09 21:19:37 +0200

    Move hotel emoji to household. Close #43

--- src/Centvrio.Emoji/Objects/HouseHold.cs
+++ src/Centvrio.Emoji/Objects/HouseHold.cs
@@ -1,54 +1,35 @@
-﻿using System;
-
-namespace Centvrio.Emoji
+﻿namespace Centvrio.Emoji
 {
-    public class Hotel
+    public class HouseHold
     {
         /// <summary>
-        /// Bellhop bell 🛎️
-        /// </summary>
-        public static readonly UnicodeString BellhopBell = 0x1F6CE;
-
-        /// <summary>
         /// Door 🚪
-        /// <para/> Obsolete. Use <see cref="HouseHold.Door"/>
         /// </summary>
-        [Obsolete]
         public static readonly UnicodeString Door = 0x1F6AA;
 
         /// <summary>
         /// Bed 🛏️
-        /// <para/> Obsolete. Use <see cref="HouseHold.Bed"/>
         /// </summary>
-        [Obsolete]
         public static readonly UnicodeString Bed = 0x1F6CF;
 
         /// <summary>
         /// Couch and lamp 🛋️
-        /// <para/> Obsolete. Use <see cref="HouseHold.CouchAndLamp"/>
         /// </summary>
-        [Obsolete]
         public static readonly UnicodeString CouchAndLamp = 0x1F6CB;
 
         /// <summary>
         /// Toilet 🚽
-        /// <para/> Obsolete. Use <see cref="HouseHold.Toilet"/>
         /// </summary>
-        [Obsolete]
         public static readonly UnicodeString Toilet = 0x1F6BD;
 
         /// <summary>
         /// Shower 🚿
-        /// <para/> Obsolete. Use <see cref="HouseHold.Shower"/>
         /// </summary>
-        [Obsolete]
         public static readonly UnicodeString Shower = 0x1F6BF;
 
         /// <summary>
         /// Bathtub 🛁
-        /// <para/> Obsolete. Use <see cref="HouseHold.Bathtub"/>
         /// </summary>
-        [Obsolete]
         public static readonly UnicodeString Bathtub = 0x1F6C1;
     }
 }
